372666ce01b2a08fd3f2650563d77878c32dc37a
mariikar <mariyagalitskaya@gmail.com>
2023-07-25 17:29:13 +0200

    Добавлены файлы memo.rb, post.rb и task.rb

---
+++ link.rb
@@ -0,0 +1,21 @@
+class Link < Post
+    def initialize 
+        super
+
+        @url - '' 
+    end
+
+    def read_from_console
+        puts "Адрес ссылки:"
+        @url = STDIN.gets.chomp
+
+        puts "Что за ссылка?"
+        @text = STDIN.gets.chomp
+    end
+
+    def to_strings
+        time_string = "Создано: #{@created_at.strfrime("@Y, @m, @d, @H: @M: @S")} \n\r  \n\r"
+
+        return [@url, @text, time_string]
+    end
+end 
---
+++ post.rb
@@ -0,0 +1,41 @@
+class Post
+
+    def self.post_types
+        [Memo, Link, Task]
+    end
+
+    def self.create(type_index)
+        return post_types[type_index].new
+    end 
+
+    def initialize 
+     @created_at = Time.now
+     @text = nill
+    end 
+
+    def read_from_console
+        #todo
+    end
+
+    def to_strings
+        #todo
+    end 
+
+    def save 
+     file = File.new(file_path, "w:UTF-8")
+
+     for item in to_strings do
+        file.puts(item)
+     end
+
+     file.close
+    end
+
+    def file_path
+        current_path = File.dirname(__FILE__)
+
+        file_name = @created_at.strfrime("#{self.class.name} _@Y-@m-@d-@H-@M-@S.txt")
+
+        return current_path + "/" + file_name
+    end 
+end 
